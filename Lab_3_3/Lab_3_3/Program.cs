79654bcd46ee6c6832c755d0e7ed996a40d50fe4
Alice-Hil <88400580+Alice-Hil@users.noreply.github.com>
2022-05-12 10:57:21 +0200

    Add files via upload

---
+++ Lab_3_3/Lab_3_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 4;
+            string[] arr = { "AA", "ABA", "B", "AAA", "BBB", "CCCC"};
+            var str = arr.OrderBy(n => n).FirstOrDefault(s => s.Length == value);
+            Console.WriteLine($"Answer: {str}");
+        }
+    }
+}
